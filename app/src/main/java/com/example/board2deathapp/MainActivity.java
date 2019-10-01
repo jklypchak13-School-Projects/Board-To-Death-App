05ff40244f112c6436b8f24bcc2f3cd583697ffa
jklypchak13 <jarod.klypchak@gmail.com>
2019-10-01 11:12:01 -0400

    Added Logging Statements for live cycle methods

--- app/src/main/java/com/example/board2deathapp/MainActivity.java
+++ app/src/main/java/com/example/board2deathapp/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        Log.d("CREATE", "Successfully created the activity");
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("START", "Successfully started the activity");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("RESUME", "Successfully Resumed the activity");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("PAUSE", "Successfully paused the activity");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("STOP", "Successfully stopped the activity.");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("RESTART", "Successfully restarted the activity.");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("DESTROY", "Successfully destroyed the activity.");
+    }
 }
