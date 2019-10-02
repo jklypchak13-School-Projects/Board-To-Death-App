230d0804ad52689c48e87302a26b7114fa6e6c0d
jklypchak13 <jarod.klypchak@gmail.com>
2019-10-02 11:12:07 -0400

    Added Log In Fragment

--- app/src/main/java/com/example/board2deathapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/board2deathapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Board to Death App");
     }
 
     public LiveData<String> getText() {
