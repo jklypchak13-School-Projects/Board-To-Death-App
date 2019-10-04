349b948e91f1e39030fbcc3a78a9be6a5d6168d7
Nick Hackman <snickhackman@gmail.com>
2019-10-04 11:20:19 -0400

    Added Fragments for Signup and event listener
    - Event Listener moves between the login and signup fragments when hitting the signup button

--- app/src/main/java/com/example/board2deathapp/MainActivity.java
+++ app/src/main/java/com/example/board2deathapp/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/board2deathapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/board2deathapp/ui/home/HomeFragment.java
@@ -1,13 +1,16 @@
 package com.example.board2deathapp.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.board2deathapp.R;
@@ -26,6 +29,18 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+        final Button sign_up = root.findViewById(R.id.btnSignup);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Fragment sign_up_page = new SignUpFragment();
+                Log.d("SWITCH", "switching fragments");
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, sign_up_page);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                Log.d("SWITCHED", "switched fragments");
             }
         });
         return root;
