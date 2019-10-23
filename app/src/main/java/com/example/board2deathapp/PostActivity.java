8cfc93f5d1f9dda1f74ac24ee6230a712337e3df
Nick Hackman <snickhackman@gmail.com>
2019-10-23 11:24:56 -0400

    Fixed Weird Merge in PostActivity

--- app/src/main/java/com/example/board2deathapp/PostActivity.java
+++ app/src/main/java/com/example/board2deathapp/PostActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.board2deathapp;
 
 import android.content.Intent;
@@ -14,6 +13,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.board2deathapp.LandingActivity;
+import com.example.board2deathapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -101,96 +102,3 @@ public class PostActivity extends AppCompatActivity {
             uri = data.getData();
             imageBtn.setImageURI(uri);
         }}}
-=======
-//package com.example.board2deathapp;
-//
-//import android.content.Intent;
-//import android.net.Uri;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.ImageButton;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
-//
-//
-//public class PostActivity extends AppCompatActivity {
-//    // imports
-//    private ImageButton imageBtn; private static final int GALLERY_REQUEST_CODE = 2;
-//    private Uri uri = null; private EditText textTitle; private EditText textDesc;
-//    private Button postBtn; private StorageReference storage; private FirebaseDatabase database;
-//    private DatabaseReference databaseRef; private FirebaseAuth mAuth;
-//    private DatabaseReference mDatabaseUsers; private FirebaseUser mCurrentUser;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_post);
-//        // initializing objects
-//        postBtn = (Button)findViewById(R.id.postBtn);
-//        textDesc = (EditText)findViewById(R.id.textDesc);
-//        textTitle = (EditText)findViewById(R.id.textTitle);
-//        storage = FirebaseStorage.getInstance().getReference();
-//        databaseRef = database.getInstance().getReference().child("Blogzone");
-//        mAuth = FirebaseAuth.getInstance();
-//        mCurrentUser = mAuth.getCurrentUser();
-//        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
-//        imageBtn = (ImageButton)findViewById(R.id.imageBtn);
-//        //picking image from gallery
-//        imageBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-//                galleryIntent.setType("image/*");
-//                startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
-//            }        });
-//        // posting to Firebase
-//        postBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(PostActivity.this, "POSTING...", Toast.LENGTH_LONG).show();
-//                final String PostTitle = textTitle.getText().toString().trim();
-//                final String PostDesc = textDesc.getText().toString().trim();
-//                // do a check for empty fields
-//                if (!TextUtils.isEmpty(PostDesc) && !TextUtils.isEmpty(PostTitle)){
-//                    StorageReference filepath = storage.child("post_images").child(uri.getLastPathSegment());
-//                    filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//                        @Override
-//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                            @SuppressWarnings("VisibleForTests")
-//                            //getting the post image download url
-//                            final Uri downloadUrl = taskSnapshot.getDownloadUrl();
-//                            Toast.makeText(getApplicationContext(), "Succesfully Uploaded", Toast.LENGTH_SHORT).show();
-//                            final DatabaseReference newPost = databaseRef.push();
-//                            //adding post contents to database reference
-//                            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
-//                                @Override
-//                                public void onDataChange(DataSnapshot dataSnapshot) {
-//                                    newPost.child("title").setValue(PostTitle);
-//                                    newPost.child("desc").setValue(PostDesc);
-//                                    newPost.child("imageUrl").setValue(downloadUrl.toString());
-//                                    newPost.child("uid").setValue(mCurrentUser.getUid());
-//                                    newPost.child("username").setValue(dataSnapshot.child("name").getValue())
-//                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                                @Override
-//                                                public void onComplete(@NonNull Task<Void> task) {
-//                                                    if (task.isSuccessful()){
-//                                                        Intent intent = new Intent(PostActivity.this, MainActivity.class);
-//                                                        startActivity(intent);
-//                                                    }}});}
-//                                @Override
-//                                public void onCancelled(DatabaseError databaseError) {
-//                                } }); } }); }}});    }
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        //image from gallery result
-//        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
-//            uri = data.getData();
-//            imageBtn.setImageURI(uri);
-//        }}}
->>>>>>> b76879e73387bdf260b82eeb3c2d77897cdc8596
